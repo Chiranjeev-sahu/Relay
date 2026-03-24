8f2637745b6172ef8fc616c8e3a7b9e71f6bbe90
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2026-03-24 23:30:02 +0530

    chore: add cuid2 dependency and update asyncHandler for generic types

--- backend/src/utils/asyncHandler.ts
+++ backend/src/utils/asyncHandler.ts
@@ -1,9 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
-type AsyncController<T> = (req: T, res: Response, next: NextFunction) => Promise<any> | any;
-
 export const asyncHandler = <T extends Request>(
-  requestHandler: AsyncController<T>
+  requestHandler: (req: T, res: Response, next: NextFunction) => any
 ): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction): void => {
     Promise.resolve(requestHandler(req as T, res, next)).catch(next);
