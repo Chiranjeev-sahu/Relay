fd177418578bfa21013cbd670976087a776cad0c
Chiranjeev-sahu <chiranjeevsahu1904@gmail.com>
2026-04-06 10:33:52 +0530

    feat: initialize express server entry point in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import { app } from "./app.js";
   try {
     app.listen(process.env.PORT, () => {
       console.log("Server on port: ", process.env.PORT);
+      console.log(`URL: http://localhost:${process.env.port}`);
     });
   } catch (error) {
     console.log("Failed to start server:", error);
